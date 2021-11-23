77d91d3eca815ac92fa05ce002dafdc18a81bace
M. Ismail <60195162+ismailxmuhammad@users.noreply.github.com>
2021-11-23 08:13:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-import db
-# import gui
 import app
-#=====================MySQL DB Practice==============
-# db.dbPractice()
-#======================================================
-#=====================Tkinter GUI Practice==============
-# gui.guiPractice()
-#======================================================
 app.appPractice()
